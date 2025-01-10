b5df07f68038e2016ff436f8c756404941f2b1ea
arvilays <123064316+arvilays@users.noreply.github.com>
2025-01-10 00:06:56 -0800

    Added type display

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ const container = document.querySelector(".container");
 const teamSize = 6;
 const artStyle = "official"; // "official", "pixel"
 const showNames = true;
+const showTypes = true;
 
 function main() {
     generatePage(swordNuzlocke);
@@ -25,47 +26,73 @@ const generateRandomParty = (party, numbers) => {
     return arr;
 }
 
-const generateImagesFromParty = (party, style = "official") => {
+const generatePokemonFromParty = (party, style = "official") => {
     for (let i = 0; i < party.length; i++) {
-        let div = document.createElement("div");
-        div.classList.add("pokemon");
+        let pokemonSlot = document.createElement("div");
+        pokemonSlot.classList.add("pokemon");
+        let pokemonTitle = generateTitle(party[i]["nickname"]);
+        let pokemonImage = document.createElement("img");
 
-        let name = document.createElement("div");
-        if (showNames) {
-            name.classList.add("name");
-            if (party[i]["nickname"] == "") {
-                name.textContent = party[i]["species"].toUpperCase();
-                name.style.color = "gold";
-            }
-            else name.textContent = party[i]["nickname"];
-        }   
-
-        let img = document.createElement("img");
         fetch('https://pokeapi.co/api/v2/pokemon/' + party[i]["species"])
             .then(response => response.json())
             .then(data => {
-                if (style == "official") {
-                    if (party[i]["shiny"]) img.src = data["sprites"]["other"]["official-artwork"].front_shiny;
-                    else img.src = data["sprites"]["other"]["official-artwork"].front_default;
-                } else if (style == "pixel") {
-                    if (party[i]["shiny"]) img.src = data["sprites"].front_shiny;
-                    else img.src = data["sprites"].front_default;
-                }
+                pokemonImage.src = generateImageLink(data, party[i]["shiny"]);
+                if (showTypes) generateTypes(data, pokemonTitle);
             })
             .catch(error => {
                 console.error('Error:', error);
             });
 
-        if (showNames) div.appendChild(name);    
-        div.appendChild(img);
-        container.appendChild(div);
+        pokemonSlot.appendChild(pokemonTitle);    
+        pokemonSlot.appendChild(pokemonImage);
+        container.appendChild(pokemonSlot);
     }
 }
+
+const generateTitle = pokemonName => {
+    let name = document.createElement("div");
+    name.classList.add("title");
+    if (showNames) {
+        let name_span = document.createElement("span");
+        name.classList.add("name");
+
+        if (pokemonName == "") {
+            name_span.textContent = party[i]["species"].toUpperCase();
+            name_span.style.color = "gold";
+        }
+        else name_span.textContent = pokemonName;
+
+        if (!showTypes) name_span.style.marginRight = "0px";
+        name.appendChild(name_span);
+    }
+    return name;
+}
+
+const generateImageLink = (data, isShiny) => {
+    if (artStyle == "official") {
+        if (isShiny) return data["sprites"]["other"]["official-artwork"].front_shiny;
+        else return data["sprites"]["other"]["official-artwork"].front_default;
+    } else if (artStyle == "pixel") {
+        if (isShiny) return data["sprites"].front_shiny;
+        else return data["sprites"].front_default;
+    }
+}
+
+const generateTypes = (data, title) => {
+    for (let i = 0; i < data["types"].length; i++) {
+        let type = data["types"][i]["type"]["name"];
+        let typeIcon = document.createElement("img");
+        typeIcon.classList.add("type");
+        typeIcon.classList.add(type);
+        typeIcon.src = "./images/types/" + type + "-icon-circle.png";
+        title.appendChild(typeIcon);
+    }
+};
 
 const generatePage = (party) => {
     let randomNumbers = generateUniqueNumbers(teamSize, party.length);
     let randomParty = generateRandomParty(party, randomNumbers);
-    generateImagesFromParty(randomParty, artStyle);
+    generatePokemonFromParty(randomParty, artStyle);
 }
 
 main();
